b100a4bcf05ac3003068bb4cc1b859e79d568873
prb1337 <rebmann23@gmail.com>
2021-04-05 14:17:33 +0200

    updated bed pid values

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -513,9 +513,9 @@
     #define DEFAULT_Ki_LIST {   1.54,   1.54 }
     #define DEFAULT_Kd_LIST {  76.55,  76.55 }
   #else
-    #define DEFAULT_Kp  22.35
-    #define DEFAULT_Ki   1.80
-    #define DEFAULT_Kd  69.41
+    #define DEFAULT_Kp 22.15
+    #define DEFAULT_Ki 1.72
+    #define DEFAULT_Kd 71.50
   #endif
 #endif // PIDTEMP
 
@@ -536,7 +536,7 @@
  * heater. If your configuration is significantly different than this and you don't understand
  * the issues involved, don't use bed PID until someone else verifies that your hardware works.
  */
-//#define PIDTEMPBED
+#define PIDTEMPBED
 
 //#define BED_LIMIT_SWITCHING
 
@@ -553,9 +553,9 @@
   //#define PID_BED_DEBUG // Sends debug data to the serial port.
 
   // Creality Ender-3 Pro
-  #define DEFAULT_bedKp 50.71
-  #define DEFAULT_bedKi 9.88
-  #define DEFAULT_bedKd 173.43
+  #define DEFAULT_bedKp 16.55
+  #define DEFAULT_bedKi 0.85
+  #define DEFAULT_bedKd 215.00
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
